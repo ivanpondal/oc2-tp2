f4cba15294429adcf44d5dfc66ca364fd5dad74b
mathigatti <mathigatti@gmail.com>
2015-09-17 21:36:28 -0300

    funcionando
    El problema era que estabamos perdiendo precision al guardar un double como unsigned char dentro de un ciclo

--- filtros/blur_c.c
+++ filtros/blur_c.c
@@ -11,6 +11,11 @@ double g_sigma(float sigm, int x_pos, int y_pos){
 	return res;	
 }
 
+//dada una matrix de n*n tenemos que se repiten los numeros ((r+1)/2)
+//0, 1, 2, 3 ... n = vecinos
+//1, 3, 6, 10 = (n+2)(n+1)/2 = numeros que se repiten
+
+
 
 
 void blur_c    (
@@ -21,6 +26,29 @@ void blur_c    (
     float sigma,
     int radius)
 {
+
+	double excedente = 0;
+//Esto que esta comentado es para que el kernel sume 1 pero enrealidad no es tan importante porque ya suma casi 1, asi que el error es infimo
+/*
+	double g_acum;
+	//calculo cada rectangulo de cada extremo del kernel
+
+	for(int i = 0; i < radius; i++){
+		for(int j = 0; j < radius; j++){
+			g_acum = g_acum + g_sigma(sigma, radius - i , radius - j);		
+			}
+	}
+	g_acum = g_acum*4;
+	//calculo la cruz del kernel formada por la fila y la columna central
+	for(int i = 0; i < radius; i++){
+		g_acum = g_acum + g_sigma(sigma, radius - i, 0)*4;
+	}
+	g_acum = g_acum + g_sigma(sigma, 0, 0);
+
+	double radius_d = radius;
+
+	excedente = (1-g_acum)/((2*radius_d+1)*(2*radius_d+1));
+*/	
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
@@ -29,19 +57,23 @@ void blur_c    (
 	int pos_j = cols - radius; // arranco a radius distancia del extremo derecho
 	double gs;
 
+	
+
 	for(int contador = 0; contador < (cols*filas - 2*radius*(cols + filas) + 2*radius*radius); contador++ ){
 		
-		unsigned char blue_acum = 0;
-		unsigned char red_acum = 0;
-		unsigned char green_acum = 0;
+		double blue_acum = 0;
+		double red_acum = 0;
+		double green_acum = 0;
 		int pos_x = 0;
-		int pos_y = 0;
+		int pos_y = 0;	
+
+
 		//Recorro la zona de tamaño radius^2 alrededor de la posicion actual y calculo el valor del punto
 		for(int contador2 = 0; contador2 < n; contador2++){			
 			gs = g_sigma(sigma, pos_x - radius, pos_y - radius);
-			blue_acum = blue_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 0]*gs;
-			green_acum = green_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 1]*gs;
-			red_acum = red_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 2]*gs;
+			blue_acum = blue_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 0]*(gs + excedente);
+			green_acum = green_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 1]*(gs + excedente);
+			red_acum = red_acum + src_matrix[pos_i + pos_y - radius][4*(pos_j - pos_x + radius) + 2]*(gs + excedente);
 			
 			if(pos_x == 2*radius + 1){
 				pos_x = 0;
