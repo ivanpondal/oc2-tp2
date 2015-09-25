5ca759eee68ab30cbc0581347ac66492dfea2035
Iván Pondal <ivan.pondal@gmail.com>
2015-09-24 21:55:07 -0300

    Add script to create random noise images

---
+++ scripts/random_images_generator.py
@@ -0,0 +1,10 @@
+import Image
+from itertools import product
+from random import randint, seed
+
+img = Image.new('RGB', (128, 128), 'white')
+seed(100)
+for x,y in product(xrange(128),xrange(128)):
+    img.putpixel((x, y), (randint(0, 255), randint(0, 255), randint(0, 255)))
+## change the first parameter
+img.save('128x128.bmp', 'BMP')
