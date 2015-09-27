4aec7a464dbcc657126b44a1fc8d1a6bc452038d
mathigatti <mathigatti@gmail.com>
2015-09-27 17:07:22 -0300

    Todas las variantes de blur_asm estan acá
    Todo perfecto excepto blue_asm_v3 (la version que usa unsigned short) que no funciona bien aun, con suerte lo arreglare en los próximos ratos.

--- filtros/blur_asm.asm
+++ filtros/blur_asm.asm
@@ -1,7 +1,17 @@
+; RECORDATORIOS
+; inputs: rdi, rsi, rdx, rcx, r8, r9
+; preservar: r12, r13, r14, r15, rbx, 
+; la pila: rbp, rsp
+; devolver cosas por rax o xmmo 
+; inputs floats: xmm0, xmm1, ..., xmm7
+
 default rel
 global blur_asm
 global gauss_matrix
 
+extern kernel_impreciso_ushort
+extern kernel_impreciso_uint
+extern g_sigma
 extern malloc
 extern free
 extern pow
@@ -10,9 +20,17 @@ section .data
 	tau:	DD 6.28319
 	euler:	DD 2.71828
 	align 16
+	
 	mask_pixel_0_to_int:	DB 0, 128, 128, 128, 1, 128, 128, 128, 2, 128, 128, 128, 3, 128, 128, 128
 	mask_int_to_pixel_0:	DB 0, 4, 8, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128
 	mask_copy_0_float:		DB 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3
+	
+	mask_pixel_0_to_short:	DB 0, 128, 1, 128, 2, 128, 3, 128, 8, 128, 9, 128, 10, 128, 11, 128
+	mask_pixel_1_to_short:	DB 0, 128, 1, 128, 2, 128, 3, 128, 128, 128, 128, 128, 128, 128, 128, 128
+	mask_copy_0:			DB 0, 1, 0, 1, 0, 1, 0, 1, 8, 9, 8, 9, 8, 9, 8, 9
+	mask_copy_1:			DB 0, 1, 0, 1, 0, 1, 0, 1, 128, 128, 128, 128, 128, 128, 128, 128
+	mask_short_to_pixel_0:	DB 8, 10, 12, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128
+
 
 section .text
 
@@ -27,7 +45,9 @@ blur_asm:
 	push rbp
 
 	; Llamo a la versión de implementación con la que quiero experimentar
-	call blur_asm_v1
+	;call blur_asm_v1
+	;call blur_asm_v2
+	call blur_asm_v3
 
 	pop rbp
 	ret
@@ -57,7 +77,7 @@ blur_asm_v1:
 	shl r8, 1		; r8d = 2*radio
 
 	mov eax, r14d
-	shl eax, 2		; edx = filas*4
+	shl eax, 2		; eax = filas*4 ;me parece que esta mal eso, deberia ser cols*4
 	xor rbx, rbx
 	mov rbx, rax	; rbx = ancho en bytes
 
@@ -292,3 +312,399 @@ gauss_2d:
 	add rsp, 16
 	pop rbp
 	ret
+
+; Implementación operando con unsigned int
+blur_asm_v2:
+	push rbp
+	mov rbp,rsp
+	sub rsp, 8
+	push rdx	; filas
+	push rcx	; columnas
+	push r8		; radio
+
+	mov r12, rdi	; r12 = puntero a imagen original
+	mov r13, rsi	; r13 = puntero a imagen destino
+	mov r14d, ecx	; r14d = columnas
+	mov r15d, r8d	; r15d = radio
+	
+
+	mov ecx, r15d	; ecx = radio
+	shl rcx, 1		; ecx = radio*2
+	add rcx, 1		; ecx = radio*2+1
+	mov ebx,ecx		; ebx = radio*2+1
+	mov eax, ecx
+	mul ecx			; eax = (radio*2+1)^2 = n
+	shl rdx, 32
+	mov edx, eax
+	shl rdx, 2		; rdx = n*n*4 (float)
+
+	mov rdi,rdx
+	
+	call malloc
+	push rax		; guardo el puntero al inicio del kernel
+
+	xor rdi,rdi
+	mov edi, ebx	; antes de llamar a kernel paso n = 2*radio+1
+	xor rsi,rsi
+	mov esi, r15d	; antes de llamar a kernel paso el radio
+	mov rdx, rax	; antes de llamar a kernel paso el puntero a la matriz
+	
+	call kernel_impreciso_uint
+	pop	 rdi		; rdi = puntero al kernel
+	push rdi		; vuelvo a guardar en la pila el puntero al kernel
+
+	xor rcx,rcx
+	mov ecx,eax 	; ecx = exponente de la potencia de 2
+
+	; Tengo que calcular la posición inicial, que como comienza desde abajo la
+	; imagen, tengo que sumarle 2 veces el radio por la cantidad de bytes por fila
+	xor r8, r8
+	mov r8d, r15d
+	shl r8, 1		; r8d = 2*radio
+
+	mov eax, r14d
+	shl eax, 2		; eax = cols*4
+	xor rbx, rbx
+	mov rbx, rax	; rbx = ancho en bytes
+
+	mul r8d
+	shl rdx, 32
+	mov edx, eax
+
+	lea r12, [r12 + rdx]	; r12 = puntero iterador imagen original
+	mov rsi, r12			; rsi = puntero imagen original
+	lea r13, [r13 + rdx]	; r13 = puntero iterador imagen destino
+
+	mov r15, r8		; x = 2*radio
+	mov r9, r8		; y = 2*radio
+
+	xor r10, r10		; x_m (matriz convolución)
+	xor r11, r11		; y_m (matriz convolución)
+	pxor xmm10, xmm10	; xmm10 = acum
+	inc r8d				; r8d = n = 2*radio+1
+
+	.convolucion:
+		; Proceso pixel i
+		movd xmm8, [r12]		; xmm8 = px_i
+		movd xmm9, [rdi]		; xmm9 = g_i
+
+		movdqu xmm11, [mask_pixel_0_to_int]
+		movdqu xmm12, [mask_copy_0_float]
+
+		pshufb xmm8, xmm11	; xmm0 = [alpha|blue|green|red] px_i
+		pshufb xmm9, xmm12
+		PMULLD xmm8, xmm9
+		PADDD xmm10, xmm8 ;probar con PADDUSB o PADDD
+
+		; Preparo punteros para el siguiente pixel
+		lea r12, [r12 + 4]
+		lea rdi, [rdi + 4]
+
+		inc r10d	; x_m++
+		mov eax, r10d
+		xor rdx, rdx
+		div r8d		; x_m/n
+		cmp edx, 0
+		; Si todavía no termine de recorrer la fila de la matriz de convolución, sigo
+		jne .convolucion
+		inc r11d	; y_m++
+		mov r12, rsi
+
+		; Calculo desplazamiento y_m en imagen
+		mov eax, r11d
+		mul ebx
+		shl rdx, 32
+		mov edx, eax
+		sub r12, rdx
+
+		xor r10, r10	; x_m = 0
+		mov eax, r11d
+		xor rdx, rdx
+		div r8d			; y_m/n
+		cmp edx, 0
+		; Si todavía no recorrí todas las filas de la matriz de convolución, sigo
+		jne .convolucion
+		; Terminé la convolución para un pixel, ahora lo guardo
+		xor r11, r11		; y_m = 0
+
+		pxor xmm5,xmm5
+		vmovq xmm5,rcx
+		PSRLD xmm10,xmm5
+		pshufb xmm10, [mask_int_to_pixel_0]
+
+		mov r12, r13		; cargo puntero a pos en imagen
+		xor rbp, rbp
+		mov ebp, [rsp + 8]	; cargo radio
+
+		mov rax, rbx
+		mul rbp
+		shl rdx, 32
+		mov edx, eax		; radio*bytes por fila
+
+		sub r12, rdx		; r12 - radio*bytes por fila
+		shl rbp, 2
+		add r12, rbp		; r12 + radio*4 (tamaño de pixel)
+
+
+		movd [r12], xmm10	; Guardo pixel resultante
+
+		inc r15d			; x++
+
+		pxor xmm10, xmm10	; reinicio acumulador
+		mov rdi, [rsp]		; vuelvo al principio de la matriz
+		add rsi, 4
+		add r13, 4
+		mov r12, rsi		; avanzo el puntero de la imagen un pixel
+
+		mov eax, r15d
+		xor rdx, rdx
+		div dword [rsp + 16]	; x/ancho
+		cmp edx, 0
+		; Si no terminé de procesar toda la fila de la imagen, sigo
+		jne .convolucion
+		inc r9d				; y++
+		xor rax, rax
+		mov eax, [rsp + 8]	; radio
+		shl rax, 1			; radio*2
+		add r15d, eax		; x = x + 2*radio
+		shl rax, 2			; rax = radio*2*4
+
+		add rsi, rax		; le sumo una fila al puntero de la imagen original
+		add r13, rax		; le suma una fila al puntero de la imagen destino
+		mov r12, rsi
+
+		mov eax, r9d
+		xor rdx, rdx
+		div dword [rsp + 24]	; y/alto
+		cmp edx, 0
+		; Si no terminé de procesar todas las filas de la imagen, sigo
+		jne .convolucion
+	pop rdi		; Libero la memoria usada por la matriz
+	call free
+
+	add rsp, 32
+	pop rbp
+    ret
+
+
+
+; Implementación operando con unsigned short
+blur_asm_v3:
+	push rbp
+	mov rbp,rsp
+	sub rsp, 8
+	push rdx	; filas
+	push rcx	; columnas
+	push r8		; radio
+
+	mov r12, rdi	; r12 = puntero a imagen original
+	mov r13, rsi	; r13 = puntero a imagen destino
+	mov r14d, ecx	; r14d = columnas
+	mov r15d, r8d	; r15d = radio
+	
+
+	mov ecx, r15d	; ecx = radio
+	shl rcx, 1		; ecx = radio*2
+	add rcx, 1		; ecx = radio*2+1
+	mov ebx,ecx		; ebx = radio*2+1
+	mov eax, ecx
+	mul ecx			; eax = (radio*2+1)^2 = n
+	shl rdx, 32
+	mov edx, eax
+	shl rdx, 1		; rdx = n*n*2
+
+	mov rdi,rdx
+	
+	call malloc
+	push rax		; guardo el puntero al inicio del kernel
+
+	xor rdi,rdi
+	mov edi, ebx	; antes de llamar a kernel paso n = 2*radio+1
+	xor rsi,rsi
+	mov esi, r15d	; antes de llamar a kernel paso el radio
+	mov rdx, rax	; antes de llamar a kernel paso el puntero a la matriz
+	
+	call kernel_impreciso_ushort
+	pop	 rdi		; rdi = puntero al kernel
+	push rdi		; vuelvo a guardar en la pila el puntero al kernel
+
+	xor rcx,rcx
+	mov ecx,eax 	; ecx = exponente de la potencia de 2
+
+	; Tengo que calcular la posición inicial, que como comienza desde abajo la
+	; imagen, tengo que sumarle 2 veces el radio por la cantidad de bytes por fila
+	xor r8, r8
+	mov r8d, r15d
+	shl r8, 1		; r8d = 2*radio
+
+	mov eax, r14d
+	shl eax, 2		; eax = cols*4
+	xor rbx, rbx
+	mov rbx, rax	; rbx = ancho en bytes
+
+	mul r8d
+	shl rdx, 32
+	mov edx, eax
+
+	lea r12, [r12 + rdx]	; r12 = puntero iterador imagen original
+	mov rsi, r12			; rsi = puntero imagen original
+	lea r13, [r13 + rdx]	; r13 = puntero iterador imagen destino
+
+	mov r15, r8		; x = 2*radio
+	mov r9, r8		; y = 2*radio
+
+	xor r10, r10		; x_m (matriz convolución)
+	xor r11, r11		; y_m (matriz convolución)
+	pxor xmm10, xmm10	; xmm10 = acum
+	shr r8,1			; r8d = radio
+
+	xor r14,r14			
+	mov r14d,r8d		; r14d = radio
+	
+
+	inc r8d				; r8d = radio + 1
+	
+	
+;ANALIZAR ESTO
+
+	.convolucion:
+
+		; Proceso pixel_i y pixel_i+1		
+		
+		cmp edi,r14d
+		je .ultimofila
+		movd xmm8, [r12]		; xmm8 = px_i
+		PSLLDQ xmm8,8
+		lea r12, [r12 + 4]
+		movd xmm8, [r12]		; xmm8 = | 0 | 0 | px_i | 0 | 0 | px_i+1|
+
+		movd xmm9, [rdi]		; xmm9 = g_i
+		PSLLDQ xmm9,8
+		lea rdi, [rdi + 4]
+		movd xmm9, [rdi]		; xmm9 = | 0 | 0 | 0 | g_i | 0 | 0 | 0 | g_i+1 |
+		
+		movdqu xmm11, [mask_pixel_0_to_short] ; Una mitad con g_i y la otra con g_i+1
+		movdqu xmm12, [mask_copy_0] ; Una mitad con un pixel y la otra con el siguiente
+
+		pshufb xmm8, xmm11	; xmm8 = [alpha_i|blue_i|green_i|red_i|alpha_i+1|blue_i+1|green_i+1|red_i+1]
+		pshufb xmm9, xmm12	; xmm9 = [g_i	|g_i	|g_i	|g_i	|g_i+1	|g_i+1	|g_i+1	|g_i+1	]	
+		PMULLW xmm8, xmm9	; xmm8 = producto entre los de arriba
+		PADDW xmm10, xmm8
+
+		jmp .sigo
+
+		.ultimofila:
+		; Proceso pixel i y pixel_i+1
+		movd xmm8, [r12]		; xmm8 = px_i
+
+		movd xmm9, [rdi]		; xmm9 = g_i
+		
+		movdqu xmm11, [mask_pixel_1_to_short] ; <-- mitad baja con pixels pasados a short
+		movdqu xmm12, [mask_copy_1] ; <-- mitad baja con 4 g_is
+
+		pshufb xmm8, xmm11	; xmm8 = [ 0 | 0 | 0 | 0 |alpha_i|blue_i|green_i|red_i]
+		pshufb xmm9, xmm12	; xmm9 = [ 0 | 0 | 0 | 0 | g_i | g_i | g_i | g_i ]
+		PMULLW xmm8, xmm9	; xmm8 = producto entre los de arriba
+		PADDW xmm10, xmm8
+
+
+		.sigo:
+		
+		;preparo los punteros para la siguiente iteracion
+		lea r12, [r12 + 4]
+		lea rdi, [rdi + 4]
+
+
+		inc r10d	; x_m++
+
+		mov eax, r10d
+		xor rdx, rdx
+		div r8d		; x_m/n
+		cmp edx, 0
+
+		; Si todavía no termine de recorrer la fila de la matriz de convolución, sigo
+		jne .convolucion
+		inc r11d	; y_m++
+		mov r12, rsi
+
+		; Calculo desplazamiento y_m en imagen
+		mov eax, r11d
+		mul ebx
+		shl rdx, 32
+		mov edx, eax
+		sub r12, rdx
+
+		xor r10, r10	; x_m = 0
+		mov eax, r11d
+		xor rdx, rdx
+		div r8d			; y_m/n
+		cmp edx, 0
+		; Si todavía no recorrí todas las filas de la matriz de convolución, sigo
+		jne .convolucion
+		; Terminé la convolución para un pixel, ahora lo guardo
+		xor r11, r11		; y_m = 0
+
+		MOVUPS xmm7,xmm10
+		PSLLDQ xmm7,8
+		PADDW xmm10, xmm7
+
+		pxor xmm5,xmm5
+		vmovq xmm5,rcx
+		PSRLW xmm10,xmm5
+		pshufb xmm10, [mask_short_to_pixel_0] ;agarro los 4 words que estan en la parte alta y los paso a 4 byte en la parte baja
+
+		mov r12, r13		; cargo puntero a pos en imagen
+		xor rbp, rbp
+		mov ebp, [rsp + 8]	; cargo radio
+
+		mov rax, rbx
+		mul rbp
+		shl rdx, 32
+		mov edx, eax		; radio*bytes por fila
+
+		sub r12, rdx		; r12 - radio*bytes por fila
+		shl rbp, 2
+		add r12, rbp		; r12 + radio*4 (tamaño de pixel)
+
+
+		movd [r12], xmm10	; Guardo pixel resultante
+
+
+
+		inc r15d			; x++
+
+		pxor xmm10, xmm10	; reinicio acumulador
+		mov rdi, [rsp]		; vuelvo al principio de la matriz
+		add rsi, 4
+		add r13, 4
+		mov r12, rsi		; avanzo el puntero de la imagen un pixel
+
+		mov eax, r15d
+		xor rdx, rdx
+		div dword [rsp + 16]	; x/ancho
+		cmp edx, 0
+		; Si no terminé de procesar toda la fila de la imagen, sigo
+		jne .convolucion
+		inc r9d				; y++
+		xor rax, rax
+		mov eax, [rsp + 8]	; radio
+		shl rax, 1			; radio*2
+		add r15d, eax		; x = x + 2*radio
+		shl rax, 2			; rax = radio*2*4
+
+		add rsi, rax		; le sumo una fila al puntero de la imagen original
+		add r13, rax		; le suma una fila al puntero de la imagen destino
+		mov r12, rsi
+
+		mov eax, r9d
+		xor rdx, rdx
+		div dword [rsp + 24]	; y/alto
+		cmp edx, 0
+		; Si no terminé de procesar todas las filas de la imagen, sigo
+		jne .convolucion
+	pop rdi		; Libero la memoria usada por la matriz
+	call free
+
+	add rsp, 32
+	pop rbp
+    ret
